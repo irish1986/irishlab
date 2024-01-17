d6b9bddb50853ee4ef015ef128211c87a6634cff
Simon HARVEY <simon.harvey.86@gmail.com>
2024-01-17 18:57:26 -0500

    Update  pre-commit

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -18,27 +18,27 @@ terraform {
 
 # credentials.auto.tfvars
 provider "proxmox" {
-  pm_api_url        = var.proxmox_api_url
-  pm_user           = var.proxmox_user
-  pm_password       = var.proxmox_password
-  pm_parallel       = 3
-  pm_tls_insecure   = true
+  pm_api_url      = var.proxmox_api_url
+  pm_user         = var.proxmox_user
+  pm_password     = var.proxmox_password
+  pm_parallel     = 3
+  pm_tls_insecure = true
 }
 
 locals {
-  lxc_files  = fileset(".", "lxc/*.yml")
-  lxc        = { for file in local.lxc_files : basename(file) => yamldecode(file(file)) }
-  iso_files  = fileset(".", "iso/*.yml")
-  iso        = { for file in local.iso_files : basename(file) => yamldecode(file(file)) }
+  lxc_files = fileset(".", "lxc/*.yml")
+  lxc       = { for file in local.lxc_files : basename(file) => yamldecode(file(file)) }
+  iso_files = fileset(".", "iso/*.yml")
+  iso       = { for file in local.iso_files : basename(file) => yamldecode(file(file)) }
   vm_files  = fileset(".", "vm/*.yml")
   vm        = { for file in local.vm_files : basename(file) => yamldecode(file(file)) }
 }
 
 module "vm_resource" {
-  source      = "./modules/vm"
-  for_each    = local.vm
+  source   = "./modules/vm"
+  for_each = local.vm
 
-  node        = each.value.node
+  node = each.value.node
 
   id          = each.value.id
   hostname    = each.value.hostname
@@ -46,8 +46,8 @@ module "vm_resource" {
   description = each.value.description
   pool        = each.value.pool
 
-  os          = each.value.os
-  size        = each.value.size
-  ip_address  = each.value.ip_address
-  gateway     = each.value.gateway
+  os         = each.value.os
+  size       = each.value.size
+  ip_address = each.value.ip_address
+  gateway    = each.value.gateway
 }
