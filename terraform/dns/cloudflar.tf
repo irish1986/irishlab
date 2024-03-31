4814c6521dea73379bbaeafc3fe684fa3d0219da
Simon HARVEY <simon.harvey.86@gmail.com>
2024-03-30 21:05:57 -0400

    Add TF DNS template

---
+++ terraform/dns/cloudflar.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+# Create a record
+resource "cloudflare_record" "www" {
+  # ...
+}
+
+# Create a page rule
+resource "cloudflare_page_rule" "www" {
+  # ...
+}
